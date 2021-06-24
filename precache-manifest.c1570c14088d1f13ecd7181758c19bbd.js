14494472e2604bba1afd8e0b9b4b27222b88d40e
lastsaintcrucified <lastsaintcrucified@gmail.com>
2021-06-24 08:22:14 +0600

    Updates

---
+++ precache-manifest.c1570c14088d1f13ecd7181758c19bbd.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2d24819abde081bc56f35f9d8bbeb3",
+    "url": "/PortFolio1/index.html"
+  },
+  {
+    "revision": "8243e8adea793bfb7c55",
+    "url": "/PortFolio1/static/css/2.7b1f77b4.chunk.css"
+  },
+  {
+    "revision": "a9759ef2f3333c4b9188",
+    "url": "/PortFolio1/static/css/main.d12cda13.chunk.css"
+  },
+  {
+    "revision": "8243e8adea793bfb7c55",
+    "url": "/PortFolio1/static/js/2.2bc15b5e.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/PortFolio1/static/js/2.2bc15b5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9759ef2f3333c4b9188",
+    "url": "/PortFolio1/static/js/main.cb91596d.chunk.js"
+  },
+  {
+    "revision": "082cb7e0580f52a0f0dd",
+    "url": "/PortFolio1/static/js/runtime-main.4f8e38a6.js"
+  },
+  {
+    "revision": "d65e853b95b594c19673a0fce15dfaf3",
+    "url": "/PortFolio1/static/media/Chat.d65e853b.png"
+  },
+  {
+    "revision": "b5d02da116864436a062e3cbcc9bdb05",
+    "url": "/PortFolio1/static/media/Nasheed.b5d02da1.png"
+  },
+  {
+    "revision": "3d45f423d62a5020c05c928ad8772391",
+    "url": "/PortFolio1/static/media/arrow.3d45f423.svg"
+  },
+  {
+    "revision": "a74212b632dc205e2bd832acaa46a2e9",
+    "url": "/PortFolio1/static/media/arrow.a74212b6.svg"
+  },
+  {
+    "revision": "45614c6fc285b3f16f0c301f219c310b",
+    "url": "/PortFolio1/static/media/bg.45614c6f.png"
+  },
+  {
+    "revision": "04e4f259c38bea403acb2148d9f90aa0",
+    "url": "/PortFolio1/static/media/contact-info-icon1.04e4f259.svg"
+  },
+  {
+    "revision": "df4fdedf2f261b6bd88551a4f1ab92f9",
+    "url": "/PortFolio1/static/media/contact-info-icon2.df4fdedf.svg"
+  },
+  {
+    "revision": "3cf93bcfa4d4d754bc4675b37bc0361b",
+    "url": "/PortFolio1/static/media/contact-info-icon3.3cf93bcf.svg"
+  },
+  {
+    "revision": "aa1e8c9c8592f04d2d3d68c86a746696",
+    "url": "/PortFolio1/static/media/facebook.aa1e8c9c.svg"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/PortFolio1/static/media/github.1bb02710.svg"
+  },
+  {
+    "revision": "04e8e999735f35b7d85033d15e8b1e98",
+    "url": "/PortFolio1/static/media/hero.04e8e999.png"
+  },
+  {
+    "revision": "5b238434e2862c877f08572b96c0ef7b",
+    "url": "/PortFolio1/static/media/linkedin.5b238434.svg"
+  },
+  {
+    "revision": "f521cf4553d6d240c47e135be9ba7a9c",
+    "url": "/PortFolio1/static/media/mobile-close.f521cf45.svg"
+  },
+  {
+    "revision": "10823520de33dffb05e6963a0d8c7bec",
+    "url": "/PortFolio1/static/media/mobile-menu.10823520.svg"
+  },
+  {
+    "revision": "27278b88a21bcbc47c397fe0b9a17ed2",
+    "url": "/PortFolio1/static/media/person01.27278b88.png"
+  },
+  {
+    "revision": "17be7667462f51dad67a34ee5900f379",
+    "url": "/PortFolio1/static/media/preview.17be7667.png"
+  },
+  {
+    "revision": "4836e3835a5a52767c361092ef096219",
+    "url": "/PortFolio1/static/media/preview.4836e383.png"
+  },
+  {
+    "revision": "b5d02da116864436a062e3cbcc9bdb05",
+    "url": "/PortFolio1/static/media/preview.b5d02da1.png"
+  },
+  {
+    "revision": "b8c422ae3e7896ad7060dfe06a556988",
+    "url": "/PortFolio1/static/media/preview.b8c422ae.png"
+  },
+  {
+    "revision": "d65e853b95b594c19673a0fce15dfaf3",
+    "url": "/PortFolio1/static/media/preview.d65e853b.png"
+  },
+  {
+    "revision": "dc24af102d936640a84c13be6138af1d",
+    "url": "/PortFolio1/static/media/preview.dc24af10.png"
+  },
+  {
+    "revision": "b8c422ae3e7896ad7060dfe06a556988",
+    "url": "/PortFolio1/static/media/shop.b8c422ae.png"
+  }
+]);
